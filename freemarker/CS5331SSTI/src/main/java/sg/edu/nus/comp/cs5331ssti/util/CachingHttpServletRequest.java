c308ad85035161ba8bc1bf59c01eeb2f62da9839
unknown <yongxian@lam.sg>
2023-05-02 06:32:27 +0800

    Add Spring Security sanitization config

---
+++ freemarker/CS5331SSTI/src/main/java/sg/edu/nus/comp/cs5331ssti/util/CachingHttpServletRequest.java
@@ -0,0 +1,29 @@
+package sg.edu.nus.comp.cs5331ssti.util;
+
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.*;
+
+public class CachingHttpServletRequest extends HttpServletRequestWrapper {
+    private byte[] cachedBody;
+
+    public CachingHttpServletRequest(HttpServletRequest request) throws IOException {
+        super(request);
+        InputStream requestInputStream = request.getInputStream();
+        this.cachedBody = StreamUtils.copyToByteArray(requestInputStream);
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        return new CachedBodyServletInputStream(this.cachedBody);
+    }
+
+    @Override
+    public BufferedReader getReader() throws IOException {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(this.cachedBody);
+        return new BufferedReader(new InputStreamReader(byteArrayInputStream));
+    }
+}
