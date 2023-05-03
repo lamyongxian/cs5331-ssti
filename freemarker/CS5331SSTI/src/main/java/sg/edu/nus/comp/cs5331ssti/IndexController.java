57effe30caf3d8efa737a966552696b3b524285b
Lam YongXian <yongxian@lam.sg>
2023-05-03 19:03:12 +0800

    Fix Spring Security SSTI Filter setup with IaC

--- freemarker/CS5331SSTI/src/main/java/sg/edu/nus/comp/cs5331ssti/IndexController.java
+++ freemarker/CS5331SSTI/src/main/java/sg/edu/nus/comp/cs5331ssti/IndexController.java
@@ -1,10 +1,11 @@
 package sg.edu.nus.comp.cs5331ssti;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
@@ -20,9 +21,10 @@ public class IndexController {
     }
 
     @PostMapping(value = "/index")
-    public ModelAndView indexPost(@ModelAttribute("article") Article article, Model model) {
+    public ModelAndView indexPost(Article article, HttpServletRequest request, Model model) {
         ModelAndView mv = new ModelAndView ("index");
         mv.addObject("article", article);
+
         return mv;
     }
 }
